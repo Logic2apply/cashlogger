1ad0047c0ca795642ddc7b83b8087695edb7c2e5
Abhishek Kapoor <kapoorkk1980@gmail.com>
2022-03-16 16:11:27 +0530

    Configured Databases

--- cashlogger/settings.py
+++ cashlogger/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import django_heroku
+import django_heroku, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,13 +82,20 @@ WSGI_APPLICATION = 'cashlogger.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
+if config('DATABASE_TYPE') == 'sqlite3':
+    DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+elif config('DATABASE_TYPE') == 'postgres':
+    DATABASES = dj_database_url.config(default=config('DATABASE_URL'))
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
